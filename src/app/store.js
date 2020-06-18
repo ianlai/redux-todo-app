e23731545177bd8da92f9fece93efca5583fdfed
Ian <ian.explore.world@gmail.com>
2020-06-18 23:39:38 +0900

    Added the example from Redux tutorial

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { createStore } from 'redux'
+import rootReducer from '../reducers/index';
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+// export default configureStore({
+//   reducer: {
+//     counter: counterReducer,
+//   },
+// });
+
+const store = createStore(rootReducer)
+
+export default store
